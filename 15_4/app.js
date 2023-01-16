f7995708ee0871e2102c295dc3e7282229f1cbfb
Bradley Chery <Bradley434@hotmail.com>
2023-01-16 03:43:25 -0500

    Added chapter 15_4

---
+++ 15_4/app.js
@@ -0,0 +1,19 @@
+function sum(...nums){
+    return nums.reduce((sum, n) => sum + n);
+};
+
+const sumAll = (...values) => {
+    if(!values.length) return undefined;
+    return values.reduce((sum, n) => sum + n)
+};
+
+function makeFamily(parent1, parent2, ...kids){
+    return {
+        parents: [parent1, parent2],
+        children: kids.length ? kids : undefined
+    };
+};
+
+function filterByType(type, ...vals){
+    return vals.filter((v) => typeof v === type);
+}
