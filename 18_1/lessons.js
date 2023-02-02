d67f1b152d67020b264476fc65645d69acabec98
Bradley Chery <Bradley434@hotmail.com>
2023-02-02 00:55:41 -0500

    continued lesson

--- 18_1/lessons.js
+++ 18_1/lessons.js
@@ -32,7 +32,7 @@ $('a').css(tealColorStyle);
 
 /*-------- For class -------------*/
 //same like regular javascript to add a class to the DOM we link the css class to the DOM h1
-$('h1') //[h1#firstHeading.firstHeading, preObject...]
+$('h1') //[h1#firstHeading.firstHeading, preObject...] . A jquery object.
 
 //add the css class to the element on the html from Jquery. Make sure the class is created on the .css file
 $('h1').addClass('highlight'); // used on one or all h1
@@ -41,4 +41,37 @@ $('img').removeClass('highlight'); // this can be used to remove all css highlig
 $('img').toggleClass('highlight'); // this can be used to toggle the css
 
 
+/*-------------- JQUERY Method Chaining --------------- */
+//Instead of 
+let todoContainer = document.querySelector("#todo-container");
+todoContainer.style.color = "red";
+todoContainer.innerHTML = "Look at this!";
+todoContainer.addEventListener('click', function(evt){ console.log("Clicked")});
+
+// We can have
+$("#todo-container").css("color", "red").text("Look at this!").on("click", function(evt) {console.log("Clicked")});
+
+
+/* ------------ JQuery Traversal ------------------------- */
+
+$('li').eq(3) //gives use a single element from the jquery object. For example it gives us the 4th place of the index of Li
+const $specialLi = $('li').eq(3); //place it inside of a const variable 
+
+$specialLi.next(); //retrieve the next sibling of the li. So the next li after index 3 li.
+$specialLi.prev(); //retrieve the previous sibling of the li. So the previous li will display before index 3 li.
+$specialLi.parent(); // this will give us the parent of that related element 
+
+$('ul') //targeting the ul on the DOM
+$('ul').children() // this will look for direct children with that 'ul' . which will be all of the 'li'
+$('li').find('a')//looks for whatever element you need
+
+/*----------------creating , appending , removing element w/JQUERY---------------------- */
+//instead of using document and creatingElement
+document.createElement('h1');
+//let practice grabbing the 'ul' and working to add li's to it
+$('ul');
+$('ul').append('<li class="highlights"> This is how to create css in JQuery! </li>');
+//lets add a checkbox to every list in the 'ul'
+$('li').append('<input type="checkbox" />'); // add checkbox's at the end of the li's
+$('li').prepend('<input type="checkbox" />'); // add checkbox's at the beginning of the li's
 
